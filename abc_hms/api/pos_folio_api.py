cba1b08960cf279a3558d1e6f71a47fbc07ff202
darwishdev <a.darwish.dev@gmail.com>
2025-09-26 14:04:11 +0300

    sync

--- abc_hms/api/pos_folio_api.py
+++ abc_hms/api/pos_folio_api.py
@@ -1,6 +1,8 @@
 
 import frappe
 import json
+
+from pydantic import ValidationError
 from abc_hms.container import app_container
 from abc_hms.dto.pos_folio_dto import FolioListFilteredResponse, FolioUpsertRequest,  FolioListFilteredRequest
 
@@ -24,8 +26,20 @@ def folio_find(folio: str):
 @frappe.whitelist(methods=["GET"])
 def folio_list_filtered():
     try:
-        data = frappe.local.request.data
-        payload: FolioListFilteredRequest = json.loads(data or "{}")
+        args = frappe.form_dict
+        if not args.get("pos_profile"):
+            raise ValidationError("pos_profile is required")
+        payload: FolioListFilteredRequest = {
+            "pos_profile": args.get("pos_profile"),
+            "docstatus": args.get("docstatus"),
+            "reservation": args.get("reservation"),
+            "guest": args.get("guest"),
+            "room": args.get("room"),
+            "arrival_from": args.get("arrival_from"),
+            "arrival_to": args.get("arrival_to"),
+            "departure_from": args.get("departure_from"),
+            "departure_to": args.get("departure_to"),
+        }
     except Exception as e:
         raise frappe.ValidationError(f"Invalid Request {str(e)}")
 
