c55faffae234fc31238ef9b800a28b87ba21165c
darwishdev <a.darwish.dev@gmail.com>
2025-10-15 12:13:11 +0300

    BAD COMMIT FOR TEST

--- abc_hms/__init__.py
+++ abc_hms/__init__.py
@@ -1,151 +1,151 @@
-
-from abc_hms.api.property_room_date_api import (
-    room_date_bulk_upsert ,
-    room_type_availability_list,
-    room_availability_list
-)
-from abc_hms.api.property_rate_code_api import (room_type_rate_bulk_upsert ,
-    room_type_rate_bulk_upsert_json , rate_code_room_type_list)
-
-
-from abc_hms.api.pos_profile_api import (
-    profile_item_list,
-        porfile_mode_of_payment_list
-)
-from abc_hms.api.pos_restaurant_table import (
-    restaurant_table_list
-)
-from abc_hms.api.pos_folio_api import (
-    folio_item_transfer,
-    folio_list_filtered,
-    folio_upsert,
-    folio_window_upsert,
-    folio_find,
-    folio_merge,
-folio_window_list,
-    folio_insert
-
-)
-from abc_hms.api.property_setting_api import (
-    property_setting_business_date_find ,
-    property_setting_find,
-    property_setting_increase_business_date
-)
-from abc_hms.api.property_inventory_api import (
-    room_date_lookup_list ,
-    room_status_list
-)
-
-
-from abc_hms.api.pos_auth_api import (
-    cashier_login
-)
-from abc_hms.api.property_reservation_api import (
-    reservation_availability_check ,
-    ignore_and_resave,
-    reservation_sync,
-reservation_date_bulk_upsert,
-    reservation_date_list,
-    room_type_rate_list,
-    end_of_day,
-)
-
-
-
-from abc_hms.api.pos_session_api import (
-pos_session_defaults_find,
-    pos_session_find,
-    pos_session_upsert,
-    pos_session_find_for_date
-)
-from abc_hms.api.pos_device_api import (
-    printer_list_by_pos_profile
-)
-from abc_hms.api.property_api import (
-property_eod,
-# property_end_of_day,
-    enqueue_property_end_of_day,
-)
-
-
-from abc_hms.api.pos_closing_entry_api import (
-    pos_closing_entry_upsert
-)
-from abc_hms.api.pos_opening_entry_api import (
-    pos_opening_entry_upsert,
-    pos_closing_entry_from_opening
-)
-from abc_hms.api.pos_invoice_api import (
-pos_invoice_item_void,
-    pos_invoice_upsert,
-currency_list,
-currency_list_input,
-    pos_invoice_find_for_date,
-    pos_invoice_item_transfer,
-        pos_invoice_item_update_widnow
-)
-from abc_hms.api.property_room_api import (
-    room_list,
-    room_list_input
-)
-from abc_hms.api.pos_customer_api import (
-    customer_list,
-    customer_upsert
-)
-
-# re-export these functions
-__all__ = [
-    "room_list",
-    "room_list_input",
-    "pos_invoice_item_transfer",
-    "room_status_list",
-    "room_date_lookup_list",
-    "pos_opening_entry_upsert",
-    "pos_session_upsert",
-    "pos_invoice_upsert",
-    "pos_closing_entry_upsert",
-    "end_of_day",
-    "reservation_availability_check",
-    "ignore_and_resave",
-    "pos_closing_entry_from_opening",
-    "property_setting_find",
-    "property_setting_business_date_find",
-    "property_setting_increase_business_date",
-    "room_date_bulk_upsert",
-    "reservation_sync",
-    "pos_invoice_find_for_date",
-    "pos_session_defaults_find",
-    # "property_end_of_day",
-    "enqueue_property_end_of_day",
-    "restaurant_table_list",
-    "folio_find",
-    "folio_list_filtered",
-    "folio_upsert",
-    "pos_session_find_for_date",
-    "pos_invoice_item_update_widnow",
-    "folio_merge",
-    "folio_insert",
-    "folio_window_upsert",
-    "profile_item_list",
-    "property_eod",
-    "porfile_mode_of_payment_list",
-    "pos_invoice_item_void",
-    "folio_item_transfer",
-    "folio_window_list",
-    "room_type_availability_list",
-    "room_availability_list",
-    "customer_list",
-    "customer_upsert",
-    "room_type_rate_list",
-    "currency_list",
-    "reservation_date_bulk_upsert",
-    "reservation_date_list",
-    "pos_session_find",
-    "currency_list_input",
-    "rate_code_room_type_list",
-    "room_type_rate_bulk_upsert_json",
-    "printer_list_by_pos_profile",
-    "room_type_rate_bulk_upsert",
-    "cashier_login"
-]
-
+#
+# from abc_hms.api.property_room_date_api import (
+#     room_date_bulk_upsert ,
+#     room_type_availability_list,
+#     room_availability_list
+# )
+# from abc_hms.api.property_rate_code_api import (room_type_rate_bulk_upsert ,
+#     room_type_rate_bulk_upsert_json , rate_code_room_type_list)
+#
+#
+# from abc_hms.api.pos_profile_api import (
+#     profile_item_list,
+#         porfile_mode_of_payment_list
+# )
+# from abc_hms.api.pos_restaurant_table import (
+#     restaurant_table_list
+# )
+# from abc_hms.api.pos_folio_api import (
+#     folio_item_transfer,
+#     folio_list_filtered,
+#     folio_upsert,
+#     folio_window_upsert,
+#     folio_find,
+#     folio_merge,
+# folio_window_list,
+#     folio_insert
+#
+# )
+# from abc_hms.api.property_setting_api import (
+#     property_setting_business_date_find ,
+#     property_setting_find,
+#     property_setting_increase_business_date
+# )
+# from abc_hms.api.property_inventory_api import (
+#     room_date_lookup_list ,
+#     room_status_list
+# )
+#
+#
+# from abc_hms.api.pos_auth_api import (
+#     cashier_login
+# )
+# from abc_hms.api.property_reservation_api import (
+#     reservation_availability_check ,
+#     ignore_and_resave,
+#     reservation_sync,
+# reservation_date_bulk_upsert,
+#     reservation_date_list,
+#     room_type_rate_list,
+#     end_of_day,
+# )
+#
+#
+#
+# from abc_hms.api.pos_session_api import (
+# pos_session_defaults_find,
+#     pos_session_find,
+#     pos_session_upsert,
+#     pos_session_find_for_date
+# )
+# from abc_hms.api.pos_device_api import (
+#     printer_list_by_pos_profile
+# )
+# from abc_hms.api.property_api import (
+# property_eod,
+# # property_end_of_day,
+#     enqueue_property_end_of_day,
+# )
+#
+#
+# from abc_hms.api.pos_closing_entry_api import (
+#     pos_closing_entry_upsert
+# )
+# from abc_hms.api.pos_opening_entry_api import (
+#     pos_opening_entry_upsert,
+#     pos_closing_entry_from_opening
+# )
+# from abc_hms.api.pos_invoice_api import (
+# pos_invoice_item_void,
+#     pos_invoice_upsert,
+# currency_list,
+# currency_list_input,
+#     pos_invoice_find_for_date,
+#     pos_invoice_item_transfer,
+#         pos_invoice_item_update_widnow
+# )
+# from abc_hms.api.property_room_api import (
+#     room_list,
+#     room_list_input
+# )
+# from abc_hms.api.pos_customer_api import (
+#     customer_list,
+#     customer_upsert
+# )
+#
+# # re-export these functions
+# __all__ = [
+#     "room_list",
+#     "room_list_input",
+#     "pos_invoice_item_transfer",
+#     "room_status_list",
+#     "room_date_lookup_list",
+#     "pos_opening_entry_upsert",
+#     "pos_session_upsert",
+#     "pos_invoice_upsert",
+#     "pos_closing_entry_upsert",
+#     "end_of_day",
+#     "reservation_availability_check",
+#     "ignore_and_resave",
+#     "pos_closing_entry_from_opening",
+#     "property_setting_find",
+#     "property_setting_business_date_find",
+#     "property_setting_increase_business_date",
+#     "room_date_bulk_upsert",
+#     "reservation_sync",
+#     "pos_invoice_find_for_date",
+#     "pos_session_defaults_find",
+#     # "property_end_of_day",
+#     "enqueue_property_end_of_day",
+#     "restaurant_table_list",
+#     "folio_find",
+#     "folio_list_filtered",
+#     "folio_upsert",
+#     "pos_session_find_for_date",
+#     "pos_invoice_item_update_widnow",
+#     "folio_merge",
+#     "folio_insert",
+#     "folio_window_upsert",
+#     "profile_item_list",
+#     "property_eod",
+#     "porfile_mode_of_payment_list",
+#     "pos_invoice_item_void",
+#     "folio_item_transfer",
+#     "folio_window_list",
+#     "room_type_availability_list",
+#     "room_availability_list",
+#     "customer_list",
+#     "customer_upsert",
+#     "room_type_rate_list",
+#     "currency_list",
+#     "reservation_date_bulk_upsert",
+#     "reservation_date_list",
+#     "pos_session_find",
+#     "currency_list_input",
+#     "rate_code_room_type_list",
+#     "room_type_rate_bulk_upsert_json",
+#     "printer_list_by_pos_profile",
+#     "room_type_rate_bulk_upsert",
+#     "cashier_login"
+# ]
+#
